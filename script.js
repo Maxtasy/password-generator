107d7282b1901fe897d34f0ef8f4307b5068bb4b
maxtasy <maxtasy88@web.de>
2019-12-27 23:31:18 +0100

    Finish project

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
-bigLetters = [];
+const characterAmountRange = document.querySelector("#characterAmountRange");
+const characterAmountNumber = document.querySelector("#characterAmountNumber");
+
+const passwordGeneratorForm = document.querySelector("#password-generator-form");
+
+characterAmountNumber.addEventListener("input", syncCharacterAmount);
+characterAmountRange.addEventListener("input", syncCharacterAmount);
+
+passwordGeneratorForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const passwordDisplay = document.querySelector(".passwordDisplay");
+    const passwordCharacterAmount = document.querySelector("#characterAmountNumber").valueAsNumber;
+    const includeUppercase = document.querySelector("#includeUppercase").checked;
+    const includeNumbers = document.querySelector("#includeNumbers").checked;
+    const includeSymbols = document.querySelector("#includeSymbols").checked;
+
+    const password = generatePassword(passwordCharacterAmount, includeUppercase, includeNumbers, includeSymbols);
+
+    passwordDisplay.textContent = password;
+});
+
+function syncCharacterAmount(e) {
+    const value = e.target.value;
+    characterAmountRange.value = value;
+    characterAmountNumber.value = value;
+}
+
+const bigLetters = [];
 for (let i = 65; i <= 90; i++) {
     bigLetters.push(String.fromCharCode(i));
 }
 
-smallLetters = [];
+const smallLetters = [];
 for (let i = 97; i <= 122; i++) {
     smallLetters.push(String.fromCharCode(i));
 }
 
-numbers = [];
+const numbers = [];
 for (let i = 48; i <= 57; i++) {
     numbers.push(String.fromCharCode(i));
 }
 
-special = [];
+const special = [];
 for (let i = 33; i <= 47; i++) {
     special.push(String.fromCharCode(i));
 }
@@ -27,25 +54,19 @@ for (let i = 123; i <= 126; i++) {
     special.push(String.fromCharCode(i));
 }
 
-function generatePassword() {
-    const passwordOutput = document.querySelector("#passwordOutput");
-    const length = document.querySelector("#length").valueAsNumber;
-    const useCapital = document.querySelector("#useCapital").checked;
-    const useNumbers = document.querySelector("#useNumbers").checked;
-    const useSpecial = document.querySelector("#useSpecial").checked;
-
+function generatePassword(passwordCharacterAmount, includeUppercase, includeNumbers, includeSymbols) {
     let selection = smallLetters.slice();
 
-    if (useCapital) selection = selection.concat(bigLetters);
-    if (useNumbers) selection = selection.concat(numbers);
-    if (useSpecial) selection = selection.concat(special);
+    if (includeUppercase) selection = selection.concat(bigLetters);
+    if (includeNumbers) selection = selection.concat(numbers);
+    if (includeSymbols) selection = selection.concat(special);
 
-    generatedPassword = "";
+    let generatedPassword = "";
 
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < passwordCharacterAmount; i++) {
         randomNumber = Math.floor((Math.random() * selection.length));
         generatedPassword += selection[randomNumber];
     }
 
-    passwordOutput.textContent = generatedPassword;
+    return generatedPassword;
 }
